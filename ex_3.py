50d2334ebb07f1c1a5cf0cca92262d5590b02dbc
alligator3105 <alligator3105@gmail.com>
2022-01-22 00:22:43 +0200

    Initial commit

---
+++ ex_3.py
@@ -0,0 +1,41 @@
+m = [
+    ["Hello", "World"],
+    ["Brad", "came", "to", "dinner", "with", "us"],
+    ["He", "loves", "takos"]
+]
+
+rules = ["LEFT", "RIGHT", "LEFT"]
+
+limit = 18
+
+new_m = []
+new_m.append('*' * limit)
+
+s = ''
+
+for i in m:
+    for j in i:
+        if len(s) + len(j) < limit:
+            s = s + j + ' '
+        else:
+            if rules[m.index(i)] == "LEFT":
+                s = '*' + s.rstrip().ljust(limit - 2, ' ') + '*'
+            elif rules[m.index(i)] == "RIGHT":
+                s = '*' + s.rstrip().rjust(limit - 2, ' ') + '*'
+
+            new_m.append(s)
+
+            s = j + ' '
+
+    if rules[m.index(i)] == "LEFT":
+        s = '*' + s.rstrip().ljust(limit - 2, ' ') + '*'
+    elif rules[m.index(i)] == "RIGHT":
+        s = '*' + s.rstrip().rjust(limit - 2, ' ') + '*'
+
+    new_m.append(s)
+    s = ''
+
+new_m.append('*' * limit)
+
+for i in new_m:
+    print(i)
